2b0074bf0f2da36e1b1d3e9936cba672f18a26c8
buendio <bak1981@mail.ru>
2020-12-22 22:57:43 +0200

    html

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox!!!</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>список заметок!</h1>
+      <ul>
+        <li>1 first</li>
+        <li>
+          <input className="input" type="text" />
+          <button>Добавить</button>
+        </li>
+      </ul>
     </div>
   );
 }
